a12af0850c9d30ad5f2cefc851d81173783d7e84
sado <liaoyonglin@shangbanzu.gift>
2022-05-24 23:32:47 +0800

    fix index lang

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -23,17 +23,17 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by Golang',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing Golang. 
+        Extend or build your web application by reusing Golang.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
